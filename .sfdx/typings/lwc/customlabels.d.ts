71d01dc6d5109f4a02a02224035541991abd421b
Daniel Probert <daniel@hyphen8.com>
2025-06-26 16:08:50 +0100

    v1.9.5 release

--- .sfdx/typings/lwc/customlabels.d.ts
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -42,3 +42,43 @@ declare module "@salesforce/label/c.WordCountText" {
     var WordCountText: string;
     export default WordCountText;
 }
+declare module "@salesforce/label/c.H8FormCloneDuplicateFormNameMessage" {
+    var H8FormCloneDuplicateFormNameMessage: string;
+    export default H8FormCloneDuplicateFormNameMessage;
+}
+declare module "@salesforce/label/c.H8FormCloneCloneButtonLabel" {
+    var H8FormCloneCloneButtonLabel: string;
+    export default H8FormCloneCloneButtonLabel;
+}
+declare module "@salesforce/label/c.H8FormCloneHelpText" {
+    var H8FormCloneHelpText: string;
+    export default H8FormCloneHelpText;
+}
+declare module "@salesforce/label/c.H8FormCloneLoadingSpinnerText" {
+    var H8FormCloneLoadingSpinnerText: string;
+    export default H8FormCloneLoadingSpinnerText;
+}
+declare module "@salesforce/label/c.H8FormCloneCloningSpinnerText" {
+    var H8FormCloneCloningSpinnerText: string;
+    export default H8FormCloneCloningSpinnerText;
+}
+declare module "@salesforce/label/c.H8FormCloneFormSelectionLabel" {
+    var H8FormCloneFormSelectionLabel: string;
+    export default H8FormCloneFormSelectionLabel;
+}
+declare module "@salesforce/label/c.H8FormCloneFormSelectionPlaceholder" {
+    var H8FormCloneFormSelectionPlaceholder: string;
+    export default H8FormCloneFormSelectionPlaceholder;
+}
+declare module "@salesforce/label/c.H8FormCloneFormNameLabel" {
+    var H8FormCloneFormNameLabel: string;
+    export default H8FormCloneFormNameLabel;
+}
+declare module "@salesforce/label/c.H8FormCloneFormToManyCharactersText" {
+    var H8FormCloneFormToManyCharactersText: string;
+    export default H8FormCloneFormToManyCharactersText;
+}
+declare module "@salesforce/label/c.H8FormCloneFormTooFewCharactersText" {
+    var H8FormCloneFormTooFewCharactersText: string;
+    export default H8FormCloneFormTooFewCharactersText;
+}
--- force-app/main/default/lwc/h8FormCloneComponent/labels.js
+++ force-app/main/default/lwc/h8FormCloneComponent/labels.js
@@ -8,6 +8,15 @@ import formSelectionPlaceholder from '@salesforce/label/c.H8FormCloneFormSelecti
 import formNameLabel from '@salesforce/label/c.H8FormCloneFormNameLabel';
 import formToManyCharacters from '@salesforce/label/c.H8FormCloneFormToManyCharactersText';
 import formToFewCharacters from '@salesforce/label/c.H8FormCloneFormTooFewCharactersText';
+import errorEncounteredToastTitle from '@salesforce/label/c.H8FormCloneErrorEncounteredToastTitle';
+import successCloningToastTitle from '@salesforce/label/c.H8FormCloneCloningToastTitle';
+import successCloningToastMessage from '@salesforce/label/c.H8FormCloneSuccessToastMessage';
+import errorGettingFormToastMessage from '@salesforce/label/c.H8FormCloneErrorGettingFormToastTitle';
+import unknownErrorToastMessage from '@salesforce/label/c.H8FormCloneErrorUnknownErrorToastMessage';
+import deploymentErrorToastMessage from '@salesforce/label/c.H8FormCloneDeploymentErrorToastMessage';
+import cloningWithStatusTitle from '@salesforce/label/c.H8FormCloneCloningWithStatusTitle';
+import cloningWaitTitle from '@salesforce/label/c.H8FormCloneCloningWaitTitle';
+import cloneFormTitle from '@salesforce/label/c.H8FormCloneCloningTitle';
 export default {
     duplicateNameText,
     cloneButtonLabel,
@@ -18,5 +27,14 @@ export default {
     formSelectionPlaceholder,
     formNameLabel,
     formToManyCharacters,
-    formToFewCharacters
+    formToFewCharacters,
+    errorEncounteredToastTitle,
+    successCloningToastTitle,
+    successCloningToastMessage,
+    errorGettingFormToastMessage,
+    unknownErrorToastMessage,
+    deploymentErrorToastMessage,
+    cloningWithStatusTitle,
+    cloningWaitTitle,
+    cloneFormTitle
 }
