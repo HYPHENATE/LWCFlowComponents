a4377ee28ace108d660e3fb2b0a82ed20c12de12
Daniel Probert <daniel@hyphen8.com>
2026-02-06 12:04:45 +0000

    Stop tracking .sfdx directory

--- .sfdx/typings/lwc/apex/customLightningDataTableController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/customLightningDataTableController.getFieldsAndRecords" {
-  export default function getFieldsAndRecords(param: {sObjectName: any, fieldSetAPIName: any, parentIDField: any, parentId: any, whereClause: any}): Promise<any>;
-}
